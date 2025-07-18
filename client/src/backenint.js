f3b53a240c629e38a87ba1a234d65d187cb2dc8e
Ticha23 <mutwirigachanja1@gmail.com>
2025-07-18 22:18:56 +0300

    Welcome

--- client/src/backenint.js
+++ client/src/backenint.js
@@ -2,6 +2,7 @@
 // Centralized backend API interaction functions for the frontend
 
 const API_BASE = 'https://roomchats-3ccf.onrender.com';
+console.log("API_BASE in production:", API_BASE);
 
 export async function getRooms() {
   const res = await fetch(`${API_BASE}/api/rooms`);
